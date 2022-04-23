92d9c26506d946796b4b13824d74b28fac4361d6
mvmccarty <xanac@xanac.33mail.com>
2022-04-23 11:42:32 -0500

    initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+module.exports = { 
+    'shops' : [
+        {
+            'name' : 'thisstoresucks',
+            'productRegex' : /^Every /
+        },
+        {
+            'name' : 'squarebiz',
+            'productRegex' : /^100% /
+        },
+        {
+            'name' : 'itmustbemagic',
+            'productRegex' : /^Life's Better /
+        }
+
+    ],
+    'targetDir' : '../rb-reports',
+    'currencySymbol' : '$',
+    'salesReportTopListCount' : 10
+};
